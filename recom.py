f7b564d601f99cf58fe5bffa70c845035e6ddffc
AVS HRUDAI <70486258+avshrudai1@users.noreply.github.com>
2021-03-04 14:27:14 +0530

    recom.py update code as its showing errors.
    upload mistake whole code wasn't uploaded only half of the code was uploaded throwing errors as such.

--- recom.py
+++ recom.py
@@ -10,3 +10,19 @@ df = pd.read_csv(path, sep='\t', names=column_names)
   
 # Check the head of the data 
 df.head() 
+
+movie_titles = pd.read_csv('https://media.geeksforgeeks.org/wp-content/uploads/Movie_Id_Titles.csv') 
+movie_titles.head() 
+
+data = pd.merge(df, movie_titles, on='item_id') 
+data.head() 
+
+data.groupby('title')['rating'].mean().sort_values(ascending=False).head() 
+
+data.groupby('title')['rating'].count().sort_values(ascending=False).head() 
+
+ratings = pd.DataFrame(data.groupby('title')['rating'].mean())  
+  
+ratings['num of ratings'] = pd.DataFrame(data.groupby('title')['rating'].count()) 
+  
+ratings.head() 
